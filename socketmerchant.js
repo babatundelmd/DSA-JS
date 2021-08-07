ab74b73403de9b9849fd374ea5d0b9ac09a6a4bf
babatundelmd <odunayohlamidi@gmail.com>
2021-08-07 09:29:41 +0100

    1 file modified

--- socketmerchant.js
+++ socketmerchant.js
@@ -3,7 +3,7 @@ function sockMerchant(n, ar) {
   var res = 0;
   ar.sort((a, b) => a - b);
   for (let i = 0; i < n; i++) {
-    if (ar[i] == ar[i + 1]) {
+    if (ar[i] === ar[i + 1]) {
       i++;
       res++;
     }
