da225e8cb96a0753f2bba8e1c6f3d84b62157dbd
babatundelmd <odunayohlamidi@gmail.com>
2021-08-25 09:34:14 +0100

    1 file modified

--- isPrime.js
+++ isPrime.js
@@ -1,9 +1,7 @@
 const isPrime = n => {
   if (n < 2) return false;
   for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      return false;
-    }
+    if (n % i === 0) return false;
   }
   return true;
 };
