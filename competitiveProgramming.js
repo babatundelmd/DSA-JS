ab6f3c19ad7d2c00ee48294b6eec911398d59b9b
babatundelmd <odunayohlamidi@gmail.com>
2021-09-05 13:51:23 +0100

    1 file modified

--- competitiveProgramming.js
+++ competitiveProgramming.js
@@ -1,16 +1,11 @@
 function numPlayers(k, scores) {
-  const sortedScores = scores.sort(function(a, b) {return b-a})
-  let rank = 0
-  let levelUp = 0
+  const sortedScores = scores.sort((a, b) => b - a);
+  let rank = 0;
+  let levelUp = 0;
   for (let i = 0; i < sortedScores.length; i++) {
-      if (i == 0) {
-          rank = 1
-      } else if (sortedScores[i] != sortedScores[i-1] {
-          rank = i + 1
-      }
-      if (rank <= k && sortedScores[i] > 0) {
-          levelUp+=1
-      }
+    if (i == 0) rank = 1;
+    else if (sortedScores[i] != sortedScores[i - 1]) rank = i + 1;
+    if (rank <= k && sortedScores[i] > 0) levelUp += 1;
   }
-  return levelUp
+  return levelUp;
 }
