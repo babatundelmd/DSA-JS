127d4c6d1568a261297e26431ca1b8e4cf12dc5c
babatundelmd <odunayohlamidi@gmail.com>
2022-01-09 18:41:49 +0100

    1 file modified

--- reVERSE.js
+++ reVERSE.js
@@ -16,13 +16,13 @@
 
 // console.log(isPalindrome$('GYU'));
 
-// function reverse(str) {
-//   let reversed = '';
-//   for (let character of str) {
-//     reversed = character + reversed;
-//   }
-//   return reversed;
-// }
+function reverse(str) {
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
 
 function reverse(str) {
  return str.split('').reduce((rev, char) => char + rev, '')
